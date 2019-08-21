7b14cee7e62a4cccd526600bcbdb059b1654d6c0
clean-command-1851 <ed7901979t@gmail.com>
2019-08-21 23:06:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -95,3 +95,55 @@ def adding_matz
     
   programmer_hash
     
+    end
+
+adding_matz
+
+def changing_alan
+	programmer_hash =
+ 		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
+     #change what Alan Kay is :known_for to the value of the alans_new_info variable.
+     alans_new_info = "GUI"
+     
+  programmer_hash[:alan_kay][:known_for] = "GUI"
+  programmer_hash
+
+end
+
+changing_alan
+
+def adding_to_dennis
+	programmer_hash =
+ 		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  programmer_hash
+
+end
+
+adding_to_dennis
